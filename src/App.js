0317b0c0c05c055743a18c7fa581ef13aa01f157
Qing Dai <qing.dai@uzh.ch>
2024-07-14 23:10:44 +0200

    set up basic frame work

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <h1>Welcome to My Schedule App</h1>
+
+        <p>
+          Start managing your schedule effectively.
+        </p>
+      </header>
+        <main>
+            <p>This is where the content will go.</p>
+        </main>
+    </div>
+  );
+}
+
+export default App;
